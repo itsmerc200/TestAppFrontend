04f2c732490fdf7a2501abae073c5cb30bc6ec07
RC <chormalhrushikesh83@gmail.com>
2025-02-12 20:15:20 +0530

    Data is bean stored from the signup page to sql, the repeted user also shown

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './modules/auth/signup/signup.component';
+import { LoginComponent } from './modules/auth/login/login.component';
 
 export const routes: Routes = [
-    {
-        path: 'register', component: SignupComponent    
-    }
+    {path: 'register', component: SignupComponent},
+    {path: 'login', component: LoginComponent}
 ];
--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -3,6 +3,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -16,7 +17,8 @@ export class SignupComponent implements OnInit {  // Implement OnInit interface
   constructor(
     private fb: FormBuilder,
     private message: NzMessageService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   validateForm!: FormGroup;
@@ -29,7 +31,18 @@ export class SignupComponent implements OnInit {  // Implement OnInit interface
     });
   }
 
-  submitForm(): void {  // Corrected submitform to submitForm
-    // Your form submission logic here
+  submitForm() {  
+    this.authService.register(this.validateForm.value).subscribe(res => {
+        this.message.success('Registration successful!', { nzDuration: 5000 });
+         
+
+      this.router.navigateByUrl("/login"); 
+    },error => {
+        this.message.error(
+          `${error.error}`,   
+          { nzDuration: 5000 }
+        );
+      }
+    );
   }
 }
