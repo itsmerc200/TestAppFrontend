297974552785939edffebc3145c061d439ed9565
RC <chormalhrushikesh83@gmail.com>
2025-02-23 23:57:57 +0530

    Added the Html For the Test component

--- src/app/modules/admin/components/dashboard/create-test/create-test.component.ts
+++ src/app/modules/admin/components/dashboard/create-test/create-test.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-test',
   standalone: true,
-  imports: [],
+  imports: [SharedModule ],
   templateUrl: './create-test.component.html',
   styleUrl: './create-test.component.css'
 })
 export class CreateTestComponent {
 
+  constructor(private fb: FormBuilder){}
+  testForm!: FormGroup;
+
+  ngOnInit(){
+
+    this.testForm = this.fb.group({
+      title: [null, Validators.required],
+      description: [null, [Validators.required]],
+      time: [null,[Validators.required]],
+    })
+  }
 }
