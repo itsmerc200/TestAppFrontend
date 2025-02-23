771215561b90a58d53b9765ce02d04e086807d63
RC <chormalhrushikesh83@gmail.com>
2025-02-24 00:39:26 +0530

    created and called the api for the admin create test

--- src/app/modules/admin/components/dashboard/create-test/create-test.component.ts
+++ src/app/modules/admin/components/dashboard/create-test/create-test.component.ts
@@ -2,25 +2,50 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../../../shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+import { AdminService } from '../../../services/admin.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-create-test',
   standalone: true,
-  imports: [SharedModule ],
+  imports: [SharedModule],
   templateUrl: './create-test.component.html',
   styleUrl: './create-test.component.css'
 })
 export class CreateTestComponent {
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder,
+    private deviceService: AdminService,
+    private notifications: NzNotificationService,
+    private router: Router,
+
+  ) { }
   testForm!: FormGroup;
 
-  ngOnInit(){
+  ngOnInit() {
 
     this.testForm = this.fb.group({
       title: [null, Validators.required],
       description: [null, [Validators.required]],
-      time: [null,[Validators.required]],
+      time: [null, [Validators.required]],
     })
   }
+
+  submitForm(){
+    if (this.testForm.valid) {
+      this.deviceService.createTest(this.testForm.value).subscribe(res=>  {
+        this.notifications.success('SUCCESS', `Test Creates Successfully.`, {nzDuration: 5000});
+
+        this.router.navigateByUrl("/admin/dashboard");
+
+      }, error=>{
+        this.notifications.error('ERROR', `${error.error}`, {nzDuration: 5000});
+      })      
+    }
+  }
+
+
+
+
 }
