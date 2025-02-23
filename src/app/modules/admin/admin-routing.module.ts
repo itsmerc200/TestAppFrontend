90f0fea34f36fa6ac41c80d375f1a6c2000d8845
RC <chormalhrushikesh83@gmail.com>
2025-02-24 00:06:58 +0530

    updates the html and the css for the test component

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateTestComponent } from './components/dashboard/create-test/create-test.component';
 
 const routes: Routes = [
 
-  {path:'dashboard', component: DashboardComponent}
+  {path:'dashboard', component: DashboardComponent},
+  {path:'create-test', component: CreateTestComponent}
 
 ];
 
