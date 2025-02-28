c7408b3d3e7b703b8b1525c4a70009dc87012896
RC <chormalhrushikesh83@gmail.com>
2025-02-28 13:26:27 +0530

    created api, and created fields and applied the validators, added the  html and css for the created fields

--- src/app/modules/admin/components/add-question-in-test/add-question-in-test.component.ts
+++ src/app/modules/admin/components/add-question-in-test/add-question-in-test.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminService } from '../../services/admin.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-question-in-test',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './add-question-in-test.component.html',
   styleUrl: './add-question-in-test.component.css'
 })
 export class AddQuestionInTestComponent {
 
+
+  constructor(private fb: FormBuilder,
+    private adminService: AdminService,
+    private notification: NzNotificationService,
+    private router: Router,
+    private activateRoute: ActivatedRoute
+  ){}
+
+
+  id: number | null;
+  questionForm!:FormGroup;
+
+
+  ngOnInit(){
+    this.questionForm = this.fb.group({
+      questionText: [null,[Validators.required]],
+      optionA: [null,[Validators.required]],
+      optionB: [null,[Validators.required]],
+      optionC: [null,[Validators.required]],
+      optionD: [null,[Validators.required]],
+      correctOption: [null,[Validators.required]],
+     
+    });
+
+    this.id = this.activateRoute.snapshot.params["id"];
+  }
 }
